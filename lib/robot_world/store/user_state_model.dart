b79499abbc05d45d9be9c3d3cc7c45fb6796a93c
朱标 <www.1006801184@qq.com>
2020-08-28 16:15:20 +0800

    状态更新，更新ui

--- lib/robot_world/store/user_state_model.dart
+++ lib/robot_world/store/user_state_model.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import '../unitls/sp_util.dart';
 import 'package:robot_world/page_index.dart';
+
 class UserStateModel extends ChangeNotifier {
   //   调用 notifyListeners(); 依赖的widget自动刷新
   //是否登录
@@ -9,13 +10,16 @@ class UserStateModel extends ChangeNotifier {
     return isLogin;
   }
 
-  //保存登录信息
+  //保存/更新 登录信息
   savaUserInfo(LoginPerson model) async {
     String data = jsonEncode(model.toJson());
     //保存登录状态
     SpUtil.setBool(DataName.LOGINSTATE, true);
     //保存登录信息
     SpUtil.setString(DataName.PERSONINFO, data);
+
     Global.profile = model;
+    
+    notifyListeners();
   }
 }
