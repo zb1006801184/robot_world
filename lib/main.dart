cf5df9cab150c72a73c06d508416a12320630027
朱标 <www.1006801184@qq.com>
2020-08-31 14:47:50 +0800

    添加main里面的相关注释 添加报错页面

--- lib/main.dart
+++ lib/main.dart
@@ -10,60 +10,40 @@ import 'package:amap_map_fluttify/amap_map_fluttify.dart';
 
 // import 'package:flutter_i18n/flutter_i18n.dart';
 void main() async {
+  //确保初始化
   WidgetsFlutterBinding.ensureInitialized();
+
+  //初始化本地持久化对象
   await SpUtil.getInstance();
+
+  //全局常量初始化 /app入口
   Global.init().then((e) {
     debugPaintSizeEnabled = false; //调试模式
+
     runApp(Store.init(child: MyApp()));
-    // runApp(TestDemo()); //国际化
+
     SystemUiOverlayStyle style = SystemUiOverlayStyle(
         statusBarColor: Colors.transparent,
         statusBarIconBrightness: Brightness.light);
+
     SystemChrome.setSystemUIOverlayStyle(style);
   });
+  //初始化地图
   await AmapService.init(
     iosKey: 'cbe00823663a07e543d2d54430432272',
     androidKey: 'c91e45151b799ec4bd6f73508e7d28ea',
   );
+  //初始化定位
   await AmapCore.init('cbe00823663a07e543d2d54430432272');
+
   await enableFluttifyLog(false);
-}
 
-class TestDemo extends StatefulWidget {
-  @override
-  _TestDemoState createState() => new _TestDemoState();
-}
+  // 自定义报错页面
+  ErrorWidget.builder = (FlutterErrorDetails flutterErrorDetails) {
+    debugPrint(flutterErrorDetails.toString());
+    return new Center(child: new Text("App错误，快去看看报错信息吧!"));
+  };
 
-class _TestDemoState extends State<TestDemo> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // Locale myLocale = Localizations.localeOf(context);
-
-    return new MaterialApp(
-      // App名字
-      title: 'robot_world',
-      initialRoute: '/',
-      onGenerateRoute: onGenerateRoute,
-      routes: routes,
-      debugShowCheckedModeBanner: true,
-      theme: ThemeData(
-        primaryColor: Colors.blue,
-      ),
-      localizationsDelegates: [
-        S.delegate,
-        GlobalCupertinoLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate
-      ],
-      supportedLocales: S.delegate.supportedLocales,
-      // localeResolutionCallback: S.delegate.resolution(),//fallback: Locale("zh","CN")
-    );
-  }
 }
 
 class MyApp extends StatelessWidget {
@@ -76,6 +56,14 @@ class MyApp extends StatelessWidget {
       routes: routes,
       debugShowCheckedModeBanner: true, //debug标识
       navigatorKey: NavKey.navKey,
+      localizationsDelegates: [
+        S.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate
+      ],
+      supportedLocales: S.delegate.supportedLocales,
+      // localeResolutionCallback: S.delegate.resolution(),//fallback: Locale("zh","CN")
     );
   }
 }
